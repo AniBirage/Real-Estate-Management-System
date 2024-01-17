556036a83b17c2b56ff866be1bfd7cf253732b4b
AniBirage <116958390+AniBirage@users.noreply.github.com>
2024-01-18 02:29:15 +0530

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,20 @@
+</div>
+
+  <script src="<?php echo APPURL; ?>/js/jquery-3.3.1.min.js"></script>
+  <script src="<?php echo APPURL; ?>/js/jquery-migrate-3.0.1.min.js"></script>
+  <script src="<?php echo APPURL; ?>/js/jquery-ui.js"></script>
+  <script src="<?php echo APPURL; ?>/js/popper.min.js"></script>
+  <script src="<?php echo APPURL; ?>/js/bootstrap.min.js"></script>
+  <script src="<?php echo APPURL; ?>/js/owl.carousel.min.js"></script>
+  <script src="<?php echo APPURL; ?>/js/mediaelement-and-player.min.js"></script>
+  <script src="<?php echo APPURL; ?>/js/jquery.stellar.min.js"></script>
+  <script src="<?php echo APPURL; ?>/js/jquery.countdown.min.js"></script>
+  <script src="<?php echo APPURL; ?>/js/jquery.magnific-popup.min.js"></script>
+  <script src="<?php echo APPURL; ?>/js/bootstrap-datepicker.min.js"></script>
+  <script src="<?php echo APPURL; ?>/js/aos.js"></script>
+  <script src="<?php echo APPURL; ?>/js/circleaudioplayer.js"></script>
+
+  <script src="<?php echo APPURL; ?>/js/main.js"></script>
+    
+  </body>
+</html>
